38d25940872b7af0e0864168b4a33b0b7556cf52
Natã <natanpedrodasilva@gmail.com>
2024-02-10 18:29:53 -0300

    feat: initial setup

---
+++ backend/src/main/java/com/example/springdish/menu/AMenuApiForRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdish.menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AMenuApiForRestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AMenuApiForRestaurantsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/springdish/menu/AMenuApiForRestaurantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdish.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AMenuApiForRestaurantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
