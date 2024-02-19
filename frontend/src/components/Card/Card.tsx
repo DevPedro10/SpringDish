66257d4b41d210781cc17e8d6b6436e53a16f84a
Natã <natanpedrodasilva@gmail.com>
2024-02-19 19:19:57 -0300

    style: changing spaces

--- frontend/src/components/Card/Card.tsx
+++ frontend/src/components/Card/Card.tsx
@@ -11,7 +11,7 @@ import { Pencil } from "lucide-react";
 export default function Card({ title, price, image }: CardProps) {
     return (
         <div className="bg-zinc-900 rounded-lg border-2 border-gray50 w-11/12 h-72 mt-6">
-            <img className="w-11/12 h-40 object-cover rounded-lg m-auto mt-3" src="https://blog.biglar.com.br/wp-content/uploads/2023/06/iStock-1212512019.jpg" alt={title} />
+            <img className="w-11/12 h-40 object-cover rounded-lg m-auto mt-3" src={image} alt={title} />
             <div className="flex justify-between">
                 <div className="p-4 flex-col justify-center">
                     <h1 className="text-xl font-medium ">{title}</h1>
